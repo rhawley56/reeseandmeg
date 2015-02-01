d100a545f8b4cd42cde04861b6ec9e25e2614a16
rhawley56 <Reese@RMBP-Server.local>
2015-02-01 15:54:46 -0600

    Updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,5 +14,6 @@ angular.module('reeseandmegApp')
       'AngularJS',
       'Karma'
     ];
-  })
-  .value('duScrollOffset', 50);
+    $scope.isCollapsed = 1;
+  });
+  //.value('duScrollOffset', 50);
