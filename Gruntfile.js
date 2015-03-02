9bc7f2e9aacc6a891894cfcdb227e95b1ca92eb5
rhawley56 <reese.hawley@gmail.com>
2015-03-02 02:09:58 -0600

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -446,7 +446,7 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
-  grunt.registerTask('heroku:production, [
+  grunt.registerTask('heroku:production', [
     'build'
   ]);
 };
