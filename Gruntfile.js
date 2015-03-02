e156c19854312eb13a06341bffa6ff24b08db82e
rhawley56 <reese.hawley@gmail.com>
2015-03-01 23:54:34 -0600

    Fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -352,9 +352,9 @@ module.exports = function (grunt) {
           src: ['generated/*']
         }, {
           expand: true,
-          cwd: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap',
-          src: '*.{eot,svg,ttf,woff}',
-          dest: '<%= yeoman.dist %>/fonts'
+          cwd: '.',
+          src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
+          dest: '<%= yeoman.dist %>'
         }, {
           expand: true,
           cwd: 'bower_components/font-awesome',
